d8d7b623a1a1febc8d69820847e04de2a9780f90
bimaryan <bimaryan046@gmail.com>
2023-12-13 00:38:59 +0700

    REST API

--- api/index.php
+++ api/index.php
@@ -2,4 +2,11 @@
 $conn = new mysqli('localhost','root','', 'extroverse');
 $query = mysqli_query($conn, 'select * from events');
 $data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+// Add image URL to each item in the result
+foreach ($data as &$item) {
+    // Assuming 'cover_foto' is the column name for the image URL in your database
+    $item['cover_foto'] = 'http://192.168.0.116/extroverse/event/buat_acara/' . $item['cover_foto'];
+}
+
 echo json_encode($data);
