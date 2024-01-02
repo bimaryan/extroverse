8dc7ca53e79bfa0f423078135c9c3ecfbc59327d
bimaryan <bimaryan046@gmail.com>
2024-01-02 20:29:57 +0700

    p

--- event/event_detail/index.php
+++ event/event_detail/index.php
@@ -65,7 +65,7 @@ if (isset($_GET['event_id'])) {
                         <div class="bg-white rounded-lg shadow p-4 mt-2 w-full">
                             <div class="flex items-center justify-between">
                                 <span class="text"><?php echo $event['tiket_type']; ?></span>
-                                <button onclick="submitMidtransForm()" class="text-center focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Rp <?php echo number_format($event['harga'], 2); ?></button>
+                                <button id="checkoutButton" class="text-center focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Rp <?php echo number_format($event['harga'], 2); ?></button>
                             </div>
                         </div>
                         <div class="bg-white rounded-lg shadow p-4 mt-2 w-full">
@@ -77,6 +77,12 @@ if (isset($_GET['event_id'])) {
                 </div>
 
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script src="https://app.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-NTHAxm4kbd8HpKUk"></script>
+                <script>
+                    document.getElementById('checkoutButton').addEventListener('click', function() {
+                        snap.pay('SB-Mid-server-6Xe5piGR5BPlDXlHcWBFN01H');
+                    });
+                </script>
             </body>
 
             </html>
